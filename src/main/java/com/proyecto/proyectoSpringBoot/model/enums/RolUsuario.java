ad345d40ba2bb36770700048757fb94264183c55
TEstebanGQ <tomasestebangonzalezquintero@gmail.com>
2026-08-27 14:34:13 -0500

    feat: 🏷️ agregar enums de dominio (RolUsuario, TipoMovimiento, TipoOperacion)

---
+++ src/main/java/com/proyecto/proyectoSpringBoot/model/enums/RolUsuario.java
@@ -0,0 +1,6 @@
+package com.proyecto.proyectoSpringBoot.model.enums;
+
+public enum RolUsuario {
+    ADMIN,
+    EMPLEADO
+}
---
+++ src/main/java/com/proyecto/proyectoSpringBoot/model/enums/TipoOperacion.java
@@ -0,0 +1,7 @@
+package com.proyecto.proyectoSpringBoot.model.enums;
+
+public enum TipoOperacion {
+    INSERT,
+    UPDATE,
+    DELETE
+}

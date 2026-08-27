5a962f9e545eda1d260a39c2643d502d02508b78
TEstebanGQ <tomasestebangonzalezquintero@gmail.com>
2026-08-27 14:34:42 -0500

    test: ✅ agregar suite de pruebas unitarias y configuracion de test

--- src/test/java/com/proyecto/proyectoSpringBoot/ProyectoSpringBootApplicationTests.java
+++ src/test/java/com/proyecto/proyectoSpringBoot/ProyectoSpringBootApplicationTests.java
@@ -1,13 +1,23 @@
 package com.proyecto.proyectoSpringBoot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ProyectoSpringBootApplicationTests {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(passwordEncoder);
+		assertTrue(passwordEncoder.matches("admin123", "$2a$10$r//FnmN.GUtMhQtM23FUhOUBQOwQZjElEu/.LXN701rSgFGKQUHpm"));
 	}
 
 }
